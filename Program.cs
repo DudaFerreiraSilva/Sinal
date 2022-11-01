97217415b7d21186243ab1103be18ca83e8eae05
Eduarda Ferreira <edududaferreira10@gmail.com>
2022-11-01 18:41:41 -0300

    Resolução

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿int numero;
+
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.Write("Digite um número: ");
+
+numero = Convert.ToInt32(Console.ReadLine());
+
+Console.ResetColor();
+
+if (numero < 0)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Negativo");
+    Console.ResetColor();
+}
+else if (numero > 0)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Positivo");
+    Console.ResetColor();
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("Zero");
+    Console.ResetColor();
+}
